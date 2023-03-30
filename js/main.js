277764f9eab66b9d98da5a7774f1a30372824ddd
Niakrist <ak_88dron@inbox.ru>
2023-03-30 13:39:24 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,6 +4,6 @@ const itog = document.querySelector('.itog');
 
 houses.forEach(function(house, index) {
 	house.addEventListener('click', () => {
-		itog.textContent = 'Нажади на дом с индексом ' + index;
+		itog.textContent = 'Дом с индексом ' + index;
 	})
 })
