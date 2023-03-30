4ea476ff6ee7307bf108319a081ef0b902f93d98
Niakrist <ak_88dron@inbox.ru>
2023-03-30 10:33:50 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,77 +1,9 @@
-const $leftLinks = document.querySelectorAll('.left-menu a'),
-			$mapLinks = document.querySelectorAll('.map a'),
-			$info = document.querySelector('.info');
+const houses = document.querySelectorAll('.test');
+const itog = document.querySelector('.itog');
 
-const requestData = (id = 1) => {
-	fetch('data.json')
-	.then((response) => {
-		return response.json();
+
+houses.forEach(function(house, index) {
+	house.addEventListener('click', () => {
+		itog.textContent = 'Нажади на дом с индексом ' + index;
 	})
-	.then((data) => {
-		$info.innerHTML = `
-			<h2>${data[id - 1].district}</h2>
-			<p>${data[id - 1].info}</p>
-		`;
-	});
-};
-
-requestData();
-
-$leftLinks.forEach(el => {
-	el.addEventListener('mouseenter', (e) => {
-		let self = e.currentTarget;
-		let selfClass = self.getAttribute('href');
-		let color = self.dataset.color;
-		let currentElement = document.querySelector(`.map a[href="${selfClass}"]`);
-		let currentPolygon = currentElement.querySelectorAll('polygon');
-		let currentPath = currentElement.querySelectorAll('path');
-		if (currentPolygon) currentPolygon.forEach(el => el.style.cssText = `fill: ${color}; stroke-width: 2px;`);
-		if (currentPath) currentPath.forEach(el => el.style.cssText = `fill: ${color}; stroke-width: 2px;`);
-		self.classList.add('active');
-	});
-
-	el.addEventListener('mouseleave', (e) => {
-		let self = e.currentTarget;
-		let selfClass = self.getAttribute('href');
-		let currentElement = document.querySelector(`.map a[href="${selfClass}"]`);
-		let currentPolygon = currentElement.querySelectorAll('polygon');
-		let currentPath = currentElement.querySelectorAll('path');
-		if (currentPolygon) currentPolygon.forEach(el => el.style.cssText = ``);
-		if (currentPath) currentPath.forEach(el => el.style.cssText = ``);
-		self.classList.remove('active');
-	});
-});
-
-$mapLinks.forEach(el => {
-	el.addEventListener('mouseenter', (e) => {
-		let self = e.currentTarget;
-		let selfClass = self.getAttribute('href');
-		let color = self.dataset.color;
-		let currentElement = document.querySelector(`.left-menu a[href="${selfClass}"]`);
-		let currentPolygon = self.querySelectorAll('polygon');
-		let currentPath = self.querySelectorAll('path');
-		if (currentPolygon) currentPolygon.forEach(el => el.style.cssText = `fill: ${color}; stroke-width: 2px;`);
-		if (currentPath) currentPath.forEach(el => el.style.cssText = `fill: ${color}; stroke-width: 2px;`);
-		currentElement.classList.add('active');
-	});
-
-	el.addEventListener('mouseleave', (e) => {
-		let self = e.currentTarget;
-		let selfClass = self.getAttribute('href');
-		let currentElement = document.querySelector(`.left-menu a[href="${selfClass}"]`);
-		let currentPolygon = self.querySelectorAll('polygon');
-		let currentPath = self.querySelectorAll('path');
-		if (currentPolygon) currentPolygon.forEach(el => el.style.cssText = ``);
-		if (currentPath) currentPath.forEach(el => el.style.cssText = ``);
-		currentElement.classList.remove('active');
-	});
-
-	el.addEventListener('click', (e) => {
-		e.preventDefault();
-		let self = e.currentTarget;
-		let selfClass = self.getAttribute('href');
-		let currentElement = document.querySelector(`.left-menu a[href="${selfClass}"]`);
-		let id = parseInt(currentElement.dataset.id);
-		requestData(id);
-	});
-});
+})
